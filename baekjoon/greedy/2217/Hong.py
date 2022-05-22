f877af62e2a63114c8e364fbcdd98790ab47303a
sednjs <48376588+sednjs@users.noreply.github.com>
2022-05-22 17:30:36 +0900

    Update Hong.py

--- baekjoon/greedy/2217/Hong.py
+++ baekjoon/greedy/2217/Hong.py
@@ -1,18 +1,26 @@
-n = int(input())
+import sys
 
-count = 0
-i = 0
-while True:
-    if n % 5 == 0: 
-        count += n//5
-        break
-    else:
-        n -= 2   
-        count += 1
+k=int(input())     #로프의 개수
+arr=[]
+ans=[]
+for i in range(k):
+    arr.append(int(input()))
 
-    if n < 0: 
-        break
-if n<0:
-    print(-1)
-else:
-    print(count)
+arr.sort(reverse=True) #오름 차순 정렬 
+
+for i in range(k):
+    ans.append(arr[i]*(i+1))
+    ''' 
+    1 4 6 8 9
+    9 8 6 4 1
+    9
+    8*2
+    6*3
+    4*4
+    1*5
+    '''
+ans.sort(reverse=True)
+print(ans[0])
+    #w: 들 수 있는 최대 중량 => 구하고자 하는 것 
+               
+
